135620c013e0205b87e95096571f5fdc491ad40c
DraconMarius <113488982+DraconMarius@users.noreply.github.com>
2022-10-20 12:02:51 -0700

    functional create combo commit with Daryl

--- index.js
+++ index.js
@@ -1,7 +1,33 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const htmlBlock = (response) =>
-    ``;
+    `<!DOCTYPE html>
+    <html lang="en">
+    
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+        <title>HTML generator</title>
+    </head>
+    
+    <body>
+        <div>
+            <div class="jumbotron">
+                <h1 id="name">${response.name}</h1>
+                <h2 id="location">${response.location}</h2>
+                <p id="description"> ${response.bio}</p>
+                <h3 id="contact-header"></h3>
+                <ul id="contact-list" class="list-group">
+                    <li class="list-group-item"><a href="${response.LinkedIn}">LinkedIn</a></li>
+                    <li class="list-group-item"><a href="https://github.com/${response.gitHub}">GitHub</a></li>
+                </ul>
+            </div>
+        </div>
+    </body>
+    
+    </html>`;
 
 inquirer.prompt([
     {
@@ -29,4 +55,10 @@ inquirer.prompt([
         name: 'gitHub',
         message: "What's your gitHub username?"
     },
-])
+]).then((response) => {
+    const generateHTML = htmlBlock(response);
+
+    fs.writeFile('index.html', generateHTML, (error) => {
+        error ? console.error(error) : console.log('successfully created index.html')
+    });
+});
